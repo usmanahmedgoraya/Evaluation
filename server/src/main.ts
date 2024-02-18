ddfcc6e8deb0d735b0a75401fc6ee9248c22629b
Usman Ahmed <usman_ahmed_intern@netixsol.com>
2024-02-18 11:29:23 +0500

    cors for post request

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,14 +4,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  const options = {
+  app.enableCors({
+    allowedHeaders: ['content-type','Authorization'],
     origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
     credentials: true,
-    Headers:'*'
-  };
-  app.enableCors(options);
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3002);
 }
