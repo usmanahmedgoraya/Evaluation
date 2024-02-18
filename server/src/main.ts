caf3fbc1afd0ce5f3e4785c1fa1c5af421fa2bc6
Usman Ahmed <usman_ahmed_intern@netixsol.com>
2024-02-18 11:33:19 +0500

    Cors issue

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,12 +4,19 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+
+  // Enable global validation pipe
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Ensure CORS is allowed for localhost:3000
   app.enableCors({
-    allowedHeaders: ['content-type','Authorization'],
     origin: '*',
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
-  app.useGlobalPipes(new ValidationPipe());
+
+  // Start the application
   await app.listen(3002);
 }
+
 bootstrap();
