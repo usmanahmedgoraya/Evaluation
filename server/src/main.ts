6721db9b2e34cd1dce815b7c7b53c1f1b5976501
Usman Ahmed <usman_ahmed_intern@netixsol.com>
2024-02-18 09:12:57 +0500

    Cors error issue

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({
     allowedHeaders: '*',
-    origin: '*',
+    origin: ['http://localhost:3000', 'https://evaluation-rust.vercel.app'],
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
