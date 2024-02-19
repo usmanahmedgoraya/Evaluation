ed8689c2abeffea006634ddf70b4cb5b69d520f9
Usman Ahmed <usman_ahmed_intern@netixsol.com>
2024-02-19 16:44:22 +0500

    cors error resoleved

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,17 +5,11 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { NewsModule } from './news/news.module';
-import Joi from 'joi';
 
 @Module({
   imports: [ConfigModule.forRoot({
     envFilePath: '.env',
     isGlobal: true
-  }),ConfigModule.forRoot({
-    validationSchema: Joi.object({
-      FRONTEND_URL: Joi.string(),
-      // ...
-    }),
   }), MongooseModule.forRoot(process.env.MONGO_URI), AuthModule, NewsModule],
   controllers: [AppController],
   providers: [AppService],
