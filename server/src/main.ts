b61d7c206cf743d6ab4e018f9aeb93c5257ec20c
Usman Ahmed <usman_ahmed_intern@netixsol.com>
2024-02-20 17:45:27 +0500

    Cors Issue

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,19 +1,18 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import cors from 'cors';
+// import cors from 'cors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
 
   // Enable global validation pipe
   app.useGlobalPipes(new ValidationPipe());
 
 
-  app.use(cors({
-    origin:'https://evaluation-rust.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  }))
+  app.enableCors({
+    origin:'https://evaluation-rust.vercel.app'
+  })
 
   // Start the application
   await app.listen(3002);
