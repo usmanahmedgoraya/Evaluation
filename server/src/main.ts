e3fc5855ab75e69d489ed8b83f2f9a4da409f93b
Usman Ahmed <usman_ahmed_intern@netixsol.com>
2024-02-20 18:09:01 +0500

    ..

--- server/src/main.ts
+++ server/src/main.ts
@@ -7,16 +7,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Enable global validation pipe
-  app.useGlobalPipes(new ValidationPipe());
-
-
+  
+  
   app.enableCors({
-    origin:'https://evaluation-rust.vercel.app',
+    origin:'https://evaluation-rust.vercel.app/',
     allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'Authorization'],
     // allowedHeaders: '*',
     credentials: true,
     optionsSuccessStatus: 204,
   })
+
+  app.useGlobalPipes(new ValidationPipe());
 
   // Start the application
   await app.listen(3002);
